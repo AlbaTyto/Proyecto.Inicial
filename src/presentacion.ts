0a0d28a3ee30c43fc724a353aa39fda961e1a7c3
Alba Tyto <gperettig@gmail.com>
2023-05-06 17:04:20 -0300

    Change to TypeScript

--- src/presentacion.ts
+++ src/presentacion.ts
@@ -1,4 +1,4 @@
-function presentación( nombre, apellido ,edad){
+function presentación( nombre: string, apellido: string ,edad: number) : void{
     console.log('Hola me llamo '+nombre+' '+apellido)
     console.log('Tengo '+edad+' años.')
-}
+};
