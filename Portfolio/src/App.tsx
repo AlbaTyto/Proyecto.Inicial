6a6dc3539ee20f4590d585efdacedb3b1ecd12d4
Alba Tyto <gperettig@gmail.com>
2023-05-11 08:45:11 -0300

    body Black background

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,19 +1,22 @@
-import {BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import NavBar from "./components/NavBar.tsx";
+import NavBar from "./components/NavBar/NavBar.tsx";
 import Home from "./components/Home.tsx";
 
 function App() {
 
   return (
     <>
+      <nav>
+        <NavBar />
+      </nav>
+
       <div>
-      <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<><NavBar /><Home /></>}/>
-        </Routes>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
         </BrowserRouter>
-        
       </div>
     </>
   );
--- Portfolio/src/components/NavBar/NavBar.tsx
+++ Portfolio/src/components/NavBar/NavBar.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import n from './NavBar.module.css';
 
 export default function NavBar(): React.JSX.Element {
-  return <div>
+  return <div className={n.NavDiv}>
     <span>
         <h1>Gustavo Peretti</h1>
         </span>
