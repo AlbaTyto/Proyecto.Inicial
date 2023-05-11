d8cd1b9b430b57342d94acbde2e14f8da253857e
Alba Tyto <gperettig@gmail.com>
2023-05-11 20:45:32 -0300

    add picture to assets

--- Portfolio/src/components/Home.tsx
+++ Portfolio/src/components/Home.tsx
@@ -2,6 +2,7 @@ import h from './Home.module.css';
 import React from "react";
 
 export default function Home(): React.JSX.Element {
-  return <div className={h.homStyle}>Hola mundo
+  return <div className={h.homStyle}>
+    <img src='../assets/IMG_20230314_151421030'/>
   </div>;
 }
