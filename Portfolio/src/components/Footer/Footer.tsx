4709603abe82104d20e9a2bc7f2dd6207d45cc95
Alba Tyto <gperettig@gmail.com>
2023-05-11 22:36:57 -0300

    feat(Footer & toDo): Links to Footer and to do forward

--- Portfolio/src/components/Footer/Footer.tsx
+++ Portfolio/src/components/Footer/Footer.tsx
@@ -3,9 +3,9 @@ import f from './Footer.module.css';
 import { Link } from 'react-router-dom';
 
 export default function Footer(): React.JSX.Element {
-  return <div className={f.NavDiv}>
-      <Link to=''>Github</Link>
-      <Link to=''>LinkedIn</Link>
-      <Link to=''>Other</Link>
+  return <div className={f.footDiv}>
+      <Link className={f.footLink} to='https://github.com/AlbaTyto'>Github</Link>
+      <Link className={f.footLink} to='https://www.linkedin.com/in/gustavo-peretti-gus/'>LinkedIn</Link>
+      <Link className={f.footLink} to=''>Other</Link>
   </div>;
 }
