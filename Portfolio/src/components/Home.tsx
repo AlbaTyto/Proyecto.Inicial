c02ededd836df9f9f41266a42d720852a5edf0a4
Alba Tyto <gperettig@gmail.com>
2023-05-11 21:38:41 -0300

    feat(Home): Picture at home

--- Portfolio/src/components/Home.tsx
+++ Portfolio/src/components/Home.tsx
@@ -3,6 +3,6 @@ import React from "react";
 
 export default function Home(): React.JSX.Element {
   return <div className={h.homStyle}>
-    <img src='../assets/IMG_20230314_151421030'/>
+    <img id={h.img} src='./src/assets/IMG_20230314_151421030.jpg' alt='Picture'/>
   </div>;
 }
