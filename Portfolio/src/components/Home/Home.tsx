0c2a2ffe68912cac2d1a609f5fe6964b58e077ba
Alba Tyto <gperettig@gmail.com>
2023-05-11 22:08:09 -0300

    feat(Home): chakra to image

--- Portfolio/src/components/Home/Home.tsx
+++ Portfolio/src/components/Home/Home.tsx
@@ -1,8 +1,14 @@
 import h from './Home.module.css';
 import React from "react";
+import { Image } from '@chakra-ui/react';
 
 export default function Home(): React.JSX.Element {
   return <div className={h.homStyle}>
-    <img id={h.img} src='./src/assets/IMG_20230314_151421030.jpg' alt='Picture'/>
+    <Image
+     src='./src/assets/IMG_20230314_151421030.jpg'
+     boxSize='35%'
+     borderRadius='full'
+     fallbackSrc='https://via.placeholder.com/450'
+     alt="'Gustavo's Picture" />
   </div>;
 }
