a9fa76791f57dd739e9ee4a79fc3b5e3f955d3ee
Alba Tyto <gperettig@gmail.com>
2023-06-08 19:50:28 -0300

    feat(home): if weith is small direction is column

--- Portfolio/src/components/Home/Home.tsx
+++ Portfolio/src/components/Home/Home.tsx
@@ -1,18 +1,24 @@
-import h from './Home.module.css';
+// import h from './Home.module.css';
 import React from "react";
-import { Image } from '@chakra-ui/react';
+import { Flex, Image } from '@chakra-ui/react';
 import Projects from '../subcomponents/Projects/Projects';
 import Skills from '../subcomponents/Skills/Skills';
 
 export default function Home(): React.JSX.Element {
-  return <div className={h.homStyle}>
+  return <Flex 
+  direction={['column', 'row']}
+  padding= '1%'
+    height= 'max-content'
+    justify-content= 'space-between'
+    alignItems= 'center'
+  >
     <Skills />
     <Image
      src='/IMG_20230314_151421030.jpg'
-     boxSize='30%'
+     boxSize={['100%', '30%']}
      borderRadius='full'
      fallbackSrc='https://via.placeholder.com/400'
      alt="'Gustavo's Picture" />
     <Projects />
-  </div>;
+  </Flex>;
 }
