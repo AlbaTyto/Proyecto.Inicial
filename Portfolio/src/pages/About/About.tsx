58a1753ca1d9202760ee045d53de29527b75f410
Alba Tyto <gperettig@gmail.com>
2023-07-10 07:08:02 -0300

    fix(About): Style improvement and text correction

--- Portfolio/src/pages/About/About.tsx
+++ Portfolio/src/pages/About/About.tsx
@@ -4,10 +4,9 @@ import { Text } from "@chakra-ui/react";
 
 export default function About(): React.JSX.Element {
   return <Text 
-  className={a.ab}
+  id={a.ab}
   fontSize='2xl'
   >
-    Hello, my name is Gustavo Peretti. I work as a Dairy Cattle Veterinarian and recently learned FullStack Developer skills at #SoyHenry. Now I have tech skills in JavaScript, Express, React, Redux, and more.
-Being a Vet has taught me about responsibility, teamwork, the scientific method, diagnosis, and prioritization. As I have always been a logical thinker, my friends and my brother recommended that I learn about programming. They suggested I try out SoyHenry, and I took a chance. I never thought that this new world could be so thrilling. This program not only gave me knowledge about software programming, but also introduced me to new friends, boosted my self-confidence, and provided me with a new perspective.Hello, my name is Gustavo Peretti. I work as a Dairy Cattle Veterinarian and recently learned FullStack Developer skills at #SoyHenry. Now I have tech skills in JavaScript, Express, React, Redux, and more. Being a Vet has taught me about responsibility, teamwork, the scientific method, diagnosis, and prioritization. As I have always been a logical thinker, my friends and my brother recommended that I learn about programming. They suggested I try out SoyHenry, and I took a chance. I never thought that this new world could be so thrilling. This program not only gave me knowledge about software programming, but also introduced me to new friends, boosted my self-confidence, and provided me with a new perspective.
+    Hello, my name is Gustavo Peretti.<br/>I work as a Dairy Cattle Veterinarian and recently learned FullStack Developer skills at #SoyHenry. Now I have tech skills in JavaScript, Express, React, Redux, and more.<br/> Being a Vet has taught me about responsibility, teamwork, the scientific method, diagnosis, and prioritization.<br/> As I have always been a logical thinker, my friends and my brother recommended that I learn about programming. They suggested I try out SoyHenry, and I took a chance. I never thought that this new world could be so thrilling. This program not only gave me knowledge about software programming, but also introduced me to new friends, boosted my self-confidence, and provided me with a new perspective.<br/>
   </Text>;
 }
